5b387e0cb58ff02a1fda40a0da4aed7981a0681f
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2024-03-24 22:14:06 -0600

    Add files via upload

--- a.c
+++ a.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
-#define PI 3.1416
 
-int main (){
-    printf("Pi vale %f" , PI);
+int main() {
+    int a = 10;
+    int b = 5;
+
+    // Suma
+    int suma = a + b;
+    printf("La suma de %d y %d es: %d\n", a, b, suma);
+
+    // Resta
+    int resta = a - b;
+    printf("La resta de %d y %d es: %d\n", a, b, resta);
+
+    // Multiplicación
+    int multiplicacion = a * b;
+    printf("El producto de %d y %d es: %d\n", a, b, multiplicacion);
+
+    // División
+    int division = a / b;
+    printf("La división de %d entre %d es: %d\n", a, b, division);
+
+    // Módulo (resto de la división)
+    int modulo = a % b;
+    printf("El módulo de %d entre %d es: %d\n", a, b, modulo);
+
     return 0;
 }
