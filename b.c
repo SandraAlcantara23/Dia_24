34e14738e6819512ac4cf1e2ea4b661149b4ef51
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2024-03-24 22:13:34 -0600

    Add files via upload

--- b.c
+++ b.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-#define PI 3.1416
 
-int main (){
-    printf("Pi vale %f" , PI);
+int main() {
+    int a = 10, b = 5;
+    printf("Suma: %d\n", a + b);
+    printf("Resta: %d\n", a - b);
+    printf("Multiplicación: %d\n", a * b);
+    printf("División: %d\n", a / b);
+    printf("Módulo: %d\n", a % b);
     return 0;
 }
